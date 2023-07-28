d886b55801163c59e7dc1c80b1f36bd48ce81c64
tare2 <tare2701@gmail.com>
2023-07-28 12:18:37 +0200

    git ignore added

---
+++ src/main/java/et/cz/tenderevaluation_backend/TenderEvaluationBackendApplication.java
@@ -0,0 +1,14 @@
+package et.cz.tenderevaluation_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenderEvaluationBackendApplication {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello dear Colleagues");
+    }
+
+}
